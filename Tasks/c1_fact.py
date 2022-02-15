d64d6d46ac3b70c69af88cb012d662c7d2abbe7a
GRISEM <dvaraz1988@gmail.com>
2022-02-15 20:35:26 +0300

    c1_fact

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -1,11 +1,17 @@
+from datetime import datetime
+
+
 def factorial_recursive(n: int) -> int:
     """
     Calculate factorial of number n (> 0) in recursive way
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    return n * factorial_recursive(n - 1)
 
 
 def factorial_iterative(n: int) -> int:
@@ -15,5 +21,16 @@ def factorial_iterative(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 1
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
+
+
+if __name__ == '__main__':
+    pass
+
