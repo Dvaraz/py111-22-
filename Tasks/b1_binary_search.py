dc5629365edeffaf421c8d247ef64b3e6ad51cee
GRISEM <dvaraz1988@gmail.com>
2022-02-15 18:45:59 +0300

    b1_binary_search

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,22 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    left_border = 0
+    right_border = len(arr) - 1
+    while left_border <= right_border:
+        middle_index = left_border + (right_border - left_border) // 2
+        middle_elem = arr[middle_index]
+        if middle_elem == elem:
+            return middle_index
+
+        if middle_elem < elem:
+            left_border = middle_index + 1
+
+        if middle_elem > elem:
+            right_border = middle_index - 1
+
+
+if __name__ == '__main__':
+    arr = [i for i in range(1, 101)]
+    print(arr)
+    print(binary_search(-1, arr))
