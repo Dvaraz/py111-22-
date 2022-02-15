8e2e7a6641cbc576b25c72545294782c7d32aa37
GRISEM <dvaraz1988@gmail.com>
2022-02-15 19:48:42 +0300

    b2_Taylor

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,6 +2,7 @@
 Taylor series
 """
 from typing import Union
+import math
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,8 +12,15 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    EPSILON = 0.000000001
+    sum = 0
+    n = 0
+    while True:
+        k = (x ** n) / (math.factorial(n))
+        n += 1
+        sum += k
+        if k < EPSILON:
+            return sum
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -24,3 +32,13 @@ def sinx(x: Union[int, float]) -> float:
     """
     print(x)
     return 0
+
+if __name__ == '__main__':
+    x = 1.553
+    n = 1
+    s = 0
+    for i in range(10):
+        k = (x ** n) / (math.factorial(n))
+        n += 1
+        s += k
+
